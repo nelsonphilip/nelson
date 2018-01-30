109a72ca75346764ae56b5815fa4ffa8d7eef448
nelsonphilip <35652437+nelsonphilip@users.noreply.github.com>
2018-01-30 16:57:34 +0530

    Create Nelson5.c

---
+++ Nelson5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>>
+void  main() 
+{
+  int x,y,z;
+  printf("Enter the values");
+  scanf("%d%d%d",&x,&y,&z);
+if(x>=y,x>=z)
+{
+    printf("Larger");
+}
+getch();")
