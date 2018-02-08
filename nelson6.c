d4b51e25e953ead0547882848cf90cbae4cfbda4
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-02-08 16:00:06 +0530

    Create nelson6.c

---
+++ nelson6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int p;
+  printf(" entr the year");
+  scanf(" %d",&p);
+  if(p%4==0)
+  {
+   printf("  its leap year \n");
+   }
+   else
+   {
+   printf(" its not leap year");
+   }
+   getch();
+    }
