aee03a03d00e8708fdd70e6df26b1dff9b023429
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-02-08 16:38:01 +0530

    Create nelson p-9.c

---
+++ nelson p-9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n,p,i ,sum=0;
+  scanf(" %d%d", &n &p");
+  printf("%d%d",n,p);
+  for(i=0;i<n,i++)
+  {
+    printf("%d",a[i]);
+  }
+  for(i=0;i<=p;i++)
+  { 
+  sum=sum+a[i];
+  }
+  printf(" \n %d",sum);
+}  
