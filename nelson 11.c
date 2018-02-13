88c2ce466689709059b0aaf19ce85623db0aa5bc
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-02-13 16:47:24 +0530

    Create nelson 11.c

---
+++ nelson 11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a,b,sum=1;
+scanf(" %d,a");
+scanf(" %d,b");
+for(i=o;i<b;i++)
+{
+  sum=sum*a;
+  }
+  
+printf(" \n %d,sum ) ;
+getch();
+}
