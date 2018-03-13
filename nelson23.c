6cccb51bb53b3808c967c233e29c7dd2bb6d002d
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-03-13 16:11:31 +0530

    Create nelson23.c

---
+++ nelson23.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],i,j,temp,n;
+printf("enter limit");
+scanf("%d",&n);
+printf("enter number");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+temp=a[0];
+for(i=0;i<n;i++)
+{
+if(temp>a[i])
+{
+temp=a[i];
+}
+
+}
+
+printf("%d",temp);
+return 0;
+}
