be77e623712f36eb950786bdd7db409ec6950767
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-03-13 16:44:28 +0530

    Create nelson30.c

---
+++ nelson30.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#inclide<conio.h>
+int main;
+{
+	int a,b,c,d,t1,t2,ans,e,f,m;
+	scanf("%d %d",&a,&b);
+	scanf("%d %d",&c,&d);
+	t1=a*60+b;
+	t2=c*60+d;
+	if(a<=0 && b<=0 && c<=0 && d<=0)
+	{
+		printf("\n%d",m);
+	}
+	else
+	{
+	if(t2>t1)
+	{
+		ans=t2-t1;
+	}
+	else
+	{
+		ans=t1-t2;
+	}
+	e=ans/60;
+	f=ans%60;
+	printf("\n%d %d",e,f);
+	}
+	return 0;
+}
