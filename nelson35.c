068116fec9d8af177bfb9efc35f130da6559d278
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-03-16 15:01:01 +0530

    Update nelson35.c

--- nelson35.c
+++ nelson35.c
@@ -2,16 +2,16 @@
 #include<conio.h>
 void main()
 {
-char str[50],i,c=0;
+char str[50],i,a=0;
 printf("enter the word");
 gets(str);
 for(i=0;str[i]!='\0';i++)
 {
 if((str[i]>='0')&&(str[i]<='9'))
 {
-c++;
+a++;
 }
 }
-printf("%d",c);
+printf("%d",a);
 getch();
 }
