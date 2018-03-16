d58816ecf0a861f89bf86182d7631d931e44974e
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-03-16 14:59:27 +0530

    Create nelson35.c

---
+++ nelson35.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50],i,c=0;
+printf("enter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if((str[i]>='0')&&(str[i]<='9'))
+{
+c++;
+}
+}
+printf("%d",c);
+getch();
+}
