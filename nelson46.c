a3636017407ca23f51bb7d13c38d1de497ad7157
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-03-20 16:23:46 +0530

    Create nelson46.c

---
+++ nelson46.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("  Enter the num");
+    scanf("%d",&a);
+    if(a!=0)
+    {
+        a=a+1;
+    }
+    printf( "the next num %d",a);
+    getch();
+}
