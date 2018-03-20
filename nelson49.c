49aac1c48606fabbc310ce42ad6f24b0555ccdd7
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-03-20 16:29:54 +0530

    Create nelson49.c

---
+++ nelson49.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("entrthe two num");
+    scanf("%d%d",&a,&b);
+    c=(a+b);
+    printf(" the sum is %d",c);
+    getch();
+}
