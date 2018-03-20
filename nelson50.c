18d7b5e9b96993c0f5f76ed3b197530b02738af8
nelson dear <35652437+nelsonphilip@users.noreply.github.com>
2018-03-20 16:35:19 +0530

    Create nelson50.c

---
+++ nelson50.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ;
+printf("Enter the num ");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the power is 2 %d",a);
+}
+else
+{
+printf(" it was not a power of two ");
+}
+}
